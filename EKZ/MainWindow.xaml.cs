dc22d16c59015e70377a99e75ae96f0b2eb5ae01
DaniilDV <daniildvornikov89@gmail.com>
2026-05-15 22:03:28 +0500

    Добавьте файлы проекта.

---
+++ EKZ/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EKZ
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Product> allProducts;
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadProducts();
+        }
+        private void LoadProducts()
+        {
+            using (var db = new EKZEntities())
+            {
+                allProducts = db.Product.ToList();
+                ProductsList.ItemsSource = allProducts;
+
+            }
+        }
+    }
+}
